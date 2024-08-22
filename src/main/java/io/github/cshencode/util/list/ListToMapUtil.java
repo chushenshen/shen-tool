1b26dc2cf5b337a7a022ad70c9f2e5803c38e0c2
css <504640003@qq.com>
2024-08-22 13:14:40 +0800

    feat: 增加 listToMap 功能

--- src/main/java/io/github/cshencode/util/list/ListToMapUtil.java
+++ src/main/java/io/github/cshencode/util/list/ListToMapUtil.java
@@ -1,5 +1,13 @@
 package io.github.cshencode.util.list;
 
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
+import io.github.cshencode.collector.CollectorUtil;
+import io.github.cshencode.util.list.fill.MFunction;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * <p>
  * -
@@ -10,15 +18,15 @@ package io.github.cshencode.util.list;
  */
 public class ListToMapUtil {
 
-    /**
-     * 如果list 是空，返回空map
-     *
-     * @param tService
-     * @param keyMapper
-     * @param <T>
-     * @param <K>
-     * @return
-     */
+//    /**
+//     * 如果list 是空，返回空map
+//     *
+//     * @param tService
+//     * @param keyMapper
+//     * @param <T>
+//     * @param <K>
+//     * @return
+//     */
 //    public static <T, K> Map<K, T> queryListToMap(Collection<K> kList, IService<T> tService, SFunction<T, K> keyMapper) {
 //        if (CollectionUtils.isEmpty(kList)) {
 //            return new HashMap<>();
@@ -32,4 +40,11 @@ public class ListToMapUtil {
 //        }
 //        return list.stream().collect(CollectorUtil.toMap(keyMapper));
 //    }
+
+    public static <T, K> Map<K, T> toMap(Collection<T> list, MFunction<T, K> keyMapper) {
+        if (CollectionUtils.isEmpty(list)) {
+            return new HashMap<>();
+        }
+        return list.stream().collect(CollectorUtil.toMap(keyMapper));
+    }
 }
